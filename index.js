ff6798e42d395bb8996570f2a3c0c11bee564560
Maxim Tokmakov <roentahl@gmail.com>
2022-10-21 01:17:23 +0300

    add chroma lib, add setElementColor for changing color of column elements depending on the column color

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Get random color for our column
+// also we can use random() method from chroma lib to get random color
 
 const cols = document.querySelectorAll('.col');
 
@@ -16,10 +17,20 @@ function generateColor() {
 function setRandomColors() {
   cols.forEach(col => {
     const colTitle = col.querySelector('.col__title');
+    const colBtn = col.querySelector('.col__btn');
     const color = generateColor();
     col.style.backgroundColor = color;
     colTitle.textContent = color;
+
+    setElementColor(colTitle, color);
+    setElementColor(colBtn, color);
   });
 };
+
+function setElementColor(element, color) {
+  const colorBrightness = chroma(color).luminance();
+
+  element.style.color = colorBrightness > 0.5 ? 'black' : 'white';
+}
 
 setRandomColors();
