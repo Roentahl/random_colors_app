c7214851268a8248f9ce313492be5aa1dfeeac27
Maxim Tokmakov <roentahl@gmail.com>
2022-10-21 23:52:24 +0300

    add block color

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-// Get random color for our column
-// also we can use random() method from chroma lib to get random color
-
 const cols = document.querySelectorAll('.col');
 
+// Get random color for our column
+// also we can use random() method from chroma lib to get random color
 function generateColor() {
   const hexCodes = '0123456789ABCDEF';
   let color = '';
@@ -16,9 +15,15 @@ function generateColor() {
 
 function setRandomColors() {
   cols.forEach(col => {
+    const isLocked = col.querySelector('i').classList.contains('fa-lock');
     const colTitle = col.querySelector('.col__title');
     const colBtn = col.querySelector('.col__btn');
     const color = generateColor();
+
+    if (isLocked) {
+      return
+    };
+
     col.style.backgroundColor = color;
     colTitle.textContent = color;
 
@@ -34,8 +39,9 @@ function setElementColor(element, color) {
   element.style.color = colorBrightness > 0.5 ? 'black' : 'white';
 };
 
-// refresh colors by space
+// refresh colors by press space
 document.addEventListener('keydown', (e) => {
+  e.preventDefault();
   if (e.code === 'Space') {
     setRandomColors();
   }
